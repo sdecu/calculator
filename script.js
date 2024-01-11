649b4f2969f7e34cc27188145da159be5c0320c5
Chris <sdecuir12@gmail.com>
2024-01-11 06:20:56 -0600

    add variable for each button

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+document.querySelector("1") = one;
+document.querySelector("2") = two;
+document.querySelector("3") = three;
+document.querySelector("4") = four;
+document.querySelector("5") = five;
+document.querySelector("6") = six;
+document.querySelector("7") = seven;
+document.querySelector("8") = eight;
+document.querySelector("9") = nine;
+document.querySelector("0") = zero;
+document.querySelector("00") = doubleZero;
+document.querySelector("ac") = ac;
+document.querySelector("delete") = erase;
+document.querySelector("%") = remainder;
+document.querySelector("/") = divide;
+document.querySelector("*") = miltiply;
+document.querySelector("-") = minus;
+document.querySelector("+") = plus;
+document.querySelector("=") = equal;
+document.querySelector(".") = decimal;
+
+
+
 function add(a, b)  {
     return a + b;
 }
